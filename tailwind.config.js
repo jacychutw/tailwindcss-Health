bba32931a678283d40534e7a31a7344929fc06d0
Jacy Chu <85614151+jacychutw@users.noreply.github.com>
2023-04-03 10:56:59 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  darkMode: 'class',
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryrose: '#F4E6E6',
+        nightrider: '#333131',
+        nightgrey: '#797979',
+        donjuan: '#605454',
+        eclipse: '#423F3F',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
